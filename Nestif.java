0d89bbb21c36fb2736dd8b0cbed5c9dd9ccc16c7
ro-hitmane <rohitam777@gmail.com>
2026-07-08 20:52:00 +0530

    Day5.2

--- Nestif.java
+++ Nestif.java
@@ -1,5 +1,8 @@
+
 import java.util.Scanner;
+
 public class Nestif {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean isStudent;
@@ -7,34 +10,30 @@ public class Nestif {
         double price = 9.99;
 
         System.out.println("Are you aStudent? (true/false): ");
-        isStudent =sc.nextBoolean();
+        isStudent = sc.nextBoolean();
 
         System.out.println("Are you a senior? (true/false): ");
         isSenior = sc.nextBoolean();
 
-
-        if(isStudent) {
-            if(isSenior) {
+        if (isStudent) {
+            if (isSenior) {
                 System.out.print("You are a senior student. You get a 20% discount.\n");
                 System.out.print("You are studnet. You get a 10% discount.\n");
                 price *= 0.7;
-            }
-            else{
+            } else {
                 System.out.print("You are studnet. You get a 10% discount.\n");
                 price *= 0.9;
 
             }
-        }
-        else {
-            if(isSenior) {
+        } else {
+            if (isSenior) {
                 System.out.print("You are a senior student. You get a 20% discount.\n");
                 price *= 0.8;
-            }
-            else {
+            } else {
                 price *= 1;
             }
         }
         System.out.printf("Your final price is: $ %.2f\n", price);
-    sc.close();
+        sc.close();
     }
 }
--- Var.java
+++ Var.java
@@ -1,5 +1,7 @@
-public class Var{
-    public static void main(String[] args){
+
+public class Var {
+
+    public static void main(String[] args) {
         //Primitive data types in java
         int age = 22;
         int year = 2026;
@@ -12,19 +14,18 @@ public class Var{
         System.out.println("$" + price + " is the price of the product and my gpa is " + gpa + " and the temperature is " + temperature);
 
         char grade = 'A';
-        char symbol ='#';
+        char symbol = '#';
         char currancy = '$';
         System.out.println(currancy + " is the currancy symbol and " + grade + " is my grade and " + symbol + " is a symbol");
-        
+
         boolean isStudent = false;
         boolean forSale = false;
         boolean isOnline = true;
         System.out.println("Is student: " + isStudent + " Is for sale: " + forSale + " Is online: " + isOnline);
 
-        if(isStudent){
+        if (isStudent) {
             System.out.println("I am a student");
-        }
-        else{
+        } else {
             System.out.println("I am not a student");
         }
 
@@ -34,6 +35,6 @@ public class Var{
         String email = "rohit@example.com";
         System.out.println("My name is " + name + " and I like to eat " + food + " and my email is " + email);
 
-System.out.println("My name is " + name.toUpperCase() + " and I like to eat " + food.toLowerCase() + " and my email is " + email.replace("example", "gmail") + "Price of puranpoli is $" + price + "it needs to be cooked at temperature " + temperature + " and my age at year " + year + " is " + age + " and I have " + quantity + " gifts" + "Is student: " + isStudent + " Is for sale: " + forSale + " Is online: " + isOnline);    
+        System.out.println("My name is " + name.toUpperCase() + " and I like to eat " + food.toLowerCase() + " and my email is " + email.replace("example", "gmail") + " Price of puranpoli is $" + price + "it needs to be cooked at temperature " + temperature + " and my age at year " + year + " is " + age + " and I have " + quantity + " gifts" + " Is student: " + isStudent + " Is for sale: " + forSale + " Is online: " + isOnline);
     }
 }
